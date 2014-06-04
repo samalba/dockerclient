e36a0a4ff2eace60426a5edc421576bf93e8cf24
Johannes 'fish' Ziemke <github@freigeist.org>
2014-06-04 19:14:00 +0200

    Update types to current types in Docker

--- types.go
+++ types.go
@@ -7,18 +7,23 @@ type ContainerConfig struct {
 	Memory          int
 	MemorySwap      int
 	CpuShares       int
+	Cpuset          string
 	AttachStdin     bool
 	AttachStdout    bool
 	AttachStderr    bool
+	PortSpecs       []string
+	ExposedPorts    map[string]struct{}
 	Tty             bool
 	OpenStdin       bool
 	StdinOnce       bool
 	Env             []string
 	Cmd             []string
 	Image           string
+	Volumes         map[string]struct{}
 	WorkingDir      string
 	Entrypoint      []string
 	NetworkDisabled bool
+	OnBuild         []string
 }
 
 type HostConfig struct {
@@ -32,6 +37,7 @@ type HostConfig struct {
 	Dns             []string
 	DnsSearch       []string
 	VolumesFrom     []string
+	NetworkMode     string
 }
 
 type PortBinding struct {
