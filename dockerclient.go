9cdaa7b419fd060c9cbfab7641c21adb0505163d
Ragnar Rova <ragnar.rova@gmail.com>
2015-02-22 00:56:50 +0100

    Make it possible to supply your own http client

--- dockerclient.go
+++ dockerclient.go
@@ -63,6 +63,10 @@ func NewDockerClientTimeout(daemonUrl string, tlsConfig *tls.Config, timeout tim
 	return &DockerClient{u, httpClient, tlsConfig, 0}, nil
 }
 
+func NewDockerClientFromHttpClient(u *url.URL, httpClient *http.Client, tlsConfig *tls.Config) (*DockerClient, error) {
+	return &DockerClient{u, httpClient, tlsConfig, 0}, nil
+}
+
 func (client *DockerClient) doRequest(method string, path string, body []byte, headers map[string]string) ([]byte, error) {
 	b := bytes.NewBuffer(body)
 	req, err := http.NewRequest(method, client.URL.String()+path, b)
