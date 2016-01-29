c0a1534a6e0f2c77d4dce06bda03728c0ffa003d
uniseraph <zhengtao.wuzt@gmail.com>
2016-01-29 11:26:14 +0800

    add Networks for ContainerInfo
    add network infos for containerInfo

--- types.go
+++ types.go
@@ -233,6 +233,8 @@ type ContainerInfo struct {
 		Gateway     string
 		Bridge      string
 		Ports       map[string][]PortBinding
+		Networks    map[string]*EndpointSettings
+
 	}
 	SysInitPath    string
 	ResolvConfPath string
